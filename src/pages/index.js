6df0b4a94ea336cc6f17d8a1f6231b909efb3c78
Matheus Amon <amonmatheus757@gmail.com>
2024-01-11 09:29:42 -0300

    Criando a segunda página da Landing Page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,18 @@
 import TemplateDefault from '@/templates/Default'
-import { Button, Container, Typography } from '@mui/material'
+import { Button, Container, Grid, Typography } from '@mui/material'
 import { makeStyles } from 'tss-react/mui'
 
 const useStyles = makeStyles()((theme) => {
   return{
-    container: {
+    containerPageOne: {
       padding: '80px 0 197px',
       backgroundImage: 'url(/images/bgTop.png)',
       backgroundSize: 'cover',
       backgroundPosition: 'center center'
+    },
+    containerPageTwo: {
+      padding: '197px 0',
+      backgroundColor: theme.palette.background.default
     },
     promise: {
       margin: '80px 0 24px',
@@ -18,14 +22,34 @@ const useStyles = makeStyles()((theme) => {
       fontWeight: 'bold'
     },
     subPromise: {
-      marginBottom: '40px',
       width: '430px',
       fontSize: '24px',
       lineHeight: '34px'
     },
     button: {
+      marginTop: '80px',
       height: '60px',
       width: '430px'
+    },
+    textWidth: {
+      width: '300px',
+    },
+    title: {
+      width: '430px',
+      fontSize: '24px',
+      fontWeight: 'bold',
+      marginBottom: '24px',
+    },
+    subTitle: {
+      fontSize: '20px',
+      fontWeight: 'bold',
+    },
+    grid: {
+      display: 'flex',
+      alignItems: 'center',
+    },
+    brainIcon: {
+      width: '250px'
     }
 
   }
@@ -36,7 +60,7 @@ const Home = () => {
 
   return (
     <TemplateDefault>
-      <Container maxWidth={`100vw`} className={classes.container}>
+      <Container maxWidth={`100vw`} className={classes.containerPageOne}>
         <Container maxWidth={'md'}>
           <img src='/images/logoHumanizar.png' />
           <Typography color={'secondary'} className={classes.promise}>
@@ -48,6 +72,93 @@ const Home = () => {
           <Button variant='contained' size='large' color='secondary' className={classes.button}>
             Quero participar
           </Button>
+        </Container>
+      </Container>
+
+      <Container maxWidth={`100vw`} className={classes.containerPageTwo}>
+        <Container maxWidth={'md'}>
+          {
+            //Grid do ícone Brain com todo conteúdo
+          }
+          <Grid container className={classes.grid}>
+            <Grid item md='6' sm='12'>
+              <img src='/images/brainIcon.png' className={classes.brainIcon}/>
+            </Grid>
+
+            <Grid item md='6' sm='12'>
+              <Typography className={ classes.title }>
+                Lorem ipsum dolor sit amet consectetur ipsum eget scelerisque
+              </Typography>
+
+              {
+                //Grid do conjunto de ícones e textos
+              }
+              <Grid container>
+                <Grid item>
+                  {
+                    //Grid do ícone e seu respectivo texto
+                  }
+                  <Grid container className={classes.grid}>
+                    <Grid item md='6' sm='12'>
+                      <img src='/images/iconConhece-te.png' />
+                    </Grid>
+                    
+                    <Grid item md='6' sm='12'>
+                      <Typography className={classes.subTitle}>
+                        Lorem ipsum
+                      </Typography>
+
+                      <Typography className={classes.textWidth}>
+                        Lorem ipsum dolor sit amet consectetur. Integer suspendisse libero aenean eu varius tortor vivamus. Risus sem egestas varius et nunc posuere.
+                      </Typography>
+                    </Grid>
+                  </Grid>
+                </Grid>
+
+                <Grid item>
+                  <Grid container className={classes.grid}>
+                    <Grid item md='6' sm='12'>
+                      <img src='/images/iconAceita-te.png' />
+                    </Grid>
+                    
+                    <Grid item md='6' sm='12'>
+                      <Typography className={classes.subTitle}>
+                        Lorem ipsum
+                      </Typography>
+
+                      <Typography className={classes.textWidth}>
+                        Lorem ipsum dolor sit amet consectetur. Integer suspendisse libero aenean eu varius tortor vivamus. Risus sem egestas varius et nunc posuere.
+                      </Typography>
+                    </Grid>
+                  </Grid> 
+                </Grid>
+
+                <Grid item>
+                  <Grid container className={classes.grid}>
+                    <Grid item md='6' sm='12'>
+                      <img src='/images/iconTransforma-te.png' />
+                    </Grid>
+                    
+                    <Grid item md='6' sm='12'>
+                      <Typography className={classes.subTitle}>
+                        Lorem ipsum
+                      </Typography>
+
+                      <Typography className={classes.textWidth}>
+                        Lorem ipsum dolor sit amet consectetur. Integer suspendisse libero aenean eu varius tortor vivamus. Risus sem egestas varius et nunc posuere.
+                      </Typography>
+                    </Grid>
+                  </Grid>
+                </Grid>
+              </Grid>
+
+              <Button variant='contained' size='large' color='secondary' className={classes.button}>
+                Quero participar
+              </Button>
+
+            </Grid>
+          </Grid>
+          
         </Container>
       </Container>
     </TemplateDefault>
