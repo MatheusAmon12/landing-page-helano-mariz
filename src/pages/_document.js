d8861639254bf69a781c77073147404fc9220949
Matheus Amon <amonmatheus757@gmail.com>
2024-01-12 21:08:18 -0300

    Instalando o Google Tag Manager

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -22,7 +22,19 @@ class MyDocument extends Document {
   render() {
     return (
       <Html lang="pt-br">
-        <Head />
+        <Head>
+          <script dangerouslySetInnerHTML={{__html: `
+            async src="https://www.googletagmanager.com/gtag/js?id=AW-345210624"
+          `}} />
+
+          <script dangerouslySetInnerHTML={{__html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+          
+            gtag('config', 'AW-345210624');
+          `}} />
+        </Head>
         <body>
           <Main />
           <NextScript />
